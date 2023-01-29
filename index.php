a78969342c8e2dc4aaf262c82da2a4efd392675e
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-01-29 15:22:27 +0000

    Require API token and add stale flag and average temperature

--- index.php
+++ index.php
@@ -11,64 +11,79 @@ function run(ServerRequestInterface $request): ResponseInterface
         'Access-Control-Allow-Methods' => 'GET, OPTIONS',
         'Content-Type' => 'application/json; charset=utf-8',
     ];
+    $timeLondon = new DateTimeImmutable('now', new DateTimeZone('Europe/London'));
+    $bodyJson = [
+        'success' => false,
+        'time_london' => $timeLondon->format('H:i (g:ia)'),
+        'timestamp' => time(),
+        'version' => getenv('K_REVISION'),
+    ];
 
     try {
-//        $timeLondon = new DateTimeImmutable('now', new DateTimeZone('Europe/London'));
-        $bodyJson = [
-            'version' => getenv('K_REVISION'),
-            'timestamp' => time(),
-//            'time_london' => $timeLondon->format('H:i (g:ia)'),
-            'devices' => [],
-        ];
-        if (!empty(getenv('SMARTTHINGS_TOKEN') && is_string(getenv('SMARTTHINGS_TOKEN')))) {
-            $token = getenv('SMARTTHINGS_TOKEN');
-            $context = stream_context_create(['http' => ['header' => [sprintf('Authorization: Bearer %s', $token)]]]);
+        $bodyJson['devices'] = [];
+        if (empty(getenv('SMARTTHINGS_TOKEN') || !is_string(getenv('SMARTTHINGS_TOKEN')))) {
+            throw new RuntimeException('Missing security token');
+        }
+        $token = getenv('SMARTTHINGS_TOKEN');
+        $context = stream_context_create(['http' => ['header' => [sprintf('Authorization: Bearer %s', $token)]]]);
 
-            $devicesWithReadingTemp = [];
+        $devicesWithReadingTemp = [];
 
-            $devicesUrl = 'https://api.smartthings.com/v1/devices/';
-            $rawDevicesData = file_get_contents($devicesUrl, false, $context);
-            $jsonDevices = json_decode($rawDevicesData, true, 512, JSON_THROW_ON_ERROR);
-            if (!empty($jsonDevices['items']) && is_array($jsonDevices['items'])) {
-                foreach ($jsonDevices['items'] as $device) {
-                    $deviceSupportsReadingTemp = false;
-                    if (is_array($device) && !empty($device['name']) && is_string($device['name']) && !empty($device['deviceId']) && is_string($device['deviceId'])  && !empty($device['components'][0]['capabilities']) && is_array($device['components'][0]['capabilities'])) {
-
-                        foreach ($device['components'][0]['capabilities'] as $capability) {
-                            if (is_array($capability) && !empty($capability['id']) && $capability['id'] === 'temperatureMeasurement') {
-                                $deviceSupportsReadingTemp = true;
-                                break;
-                            }
+        $devicesUrl = 'https://api.smartthings.com/v1/devices/';
+        $rawDevicesData = file_get_contents($devicesUrl, false, $context);
+        $jsonDevices = json_decode($rawDevicesData, true, 512, JSON_THROW_ON_ERROR);
+        if (!empty($jsonDevices['items']) && is_array($jsonDevices['items'])) {
+            foreach ($jsonDevices['items'] as $device) {
+                $deviceSupportsReadingTemp = false;
+                if (is_array($device) && !empty($device['name']) && is_string($device['name']) && !empty($device['deviceId']) && is_string($device['deviceId'])  && !empty($device['components'][0]['capabilities']) && is_array($device['components'][0]['capabilities'])) {
+                    foreach ($device['components'][0]['capabilities'] as $capability) {
+                        if (is_array($capability) && !empty($capability['id']) && $capability['id'] === 'temperatureMeasurement') {
+                            $deviceSupportsReadingTemp = true;
+                            break;
                         }
-                        if ($deviceSupportsReadingTemp) {
-                            $devicesWithReadingTemp[$device['name']] = $device['deviceId'];
-                        }
+                    }
+                    if ($deviceSupportsReadingTemp) {
+                        $devicesWithReadingTemp[$device['name']] = $device['deviceId'];
                     }
                 }
             }
+        }
 
-            foreach ($devicesWithReadingTemp as $deviceName => $deviceId) {
-                $deviceUrl = sprintf('https://api.smartthings.com/v1/devices/%s/status', $deviceId);
-                $rawDeviceData = file_get_contents($deviceUrl, false, $context);
-                $jsonDevice = json_decode($rawDeviceData, true, 512, JSON_THROW_ON_ERROR);
-
-                if (!empty($jsonDevice['components']['main']['temperatureMeasurement']['temperature']['value']) && !empty($jsonDevice['components']['main']['temperatureMeasurement']['temperature']['timestamp'])) {
-                    $temp = (float)$jsonDevice['components']['main']['temperatureMeasurement']['temperature']['value'];
-                    $timestamp = strtotime($jsonDevice['components']['main']['temperatureMeasurement']['temperature']['timestamp']);
-                    $bodyJson['devices'][] = [
-                        'name' => $deviceName,
-                        'temp' => $temp,
-                        'timestamp' => $timestamp,
-                    ];
+        $totalForAverage = 0;
+        $totalDevicesAveraged = 0;
+        foreach ($devicesWithReadingTemp as $deviceName => $deviceId) {
+            $deviceUrl = sprintf('https://api.smartthings.com/v1/devices/%s/status', $deviceId);
+            $rawDeviceData = file_get_contents($deviceUrl, false, $context);
+            $jsonDevice = json_decode($rawDeviceData, true, 512, JSON_THROW_ON_ERROR);
+            if (!empty($jsonDevice['components']['main']['temperatureMeasurement']['temperature']['value']) && !empty($jsonDevice['components']['main']['temperatureMeasurement']['temperature']['timestamp'])) {
+                $temp = (float)$jsonDevice['components']['main']['temperatureMeasurement']['temperature']['value'];
+                $timestamp = strtotime($jsonDevice['components']['main']['temperatureMeasurement']['temperature']['timestamp']);
+                $stale = $timestamp < time() - (24 * 60 * 60);
+                if (!$stale) {
+                    $totalForAverage += $temp;
+                    $totalDevicesAveraged++;
                 }
+                $bodyJson['devices'][] = [
+                    'name' => $deviceName,
+                    'temp' => $temp,
+                    'timestamp' => $timestamp,
+                    'stale' => $stale,
+                ];
             }
         }
+        if ($totalDevicesAveraged > 0) {
+            $bodyJson['averageTempForNonStale'] = $totalForAverage / $totalDevicesAveraged;
+        }
+        $bodyJson['success'] = true;
+        ksort($bodyJson);
         $headers['Surrogate-Control'] = 'max-age=180';
         $headers['Cache-Control'] = 's-maxage=180, max-age=0';
         $body = json_encode($bodyJson, JSON_THROW_ON_ERROR);
         $response = new Response(200, $headers, $body);
     } catch (Throwable $e) {
-        $response = new Response(500, $headers, 'An error occurred :(');
+        $bodyJson['error'] = 'Could not reach smart home API to get the latest data :(';
+        $body = json_encode($bodyJson, JSON_THROW_ON_ERROR);
+        $response = new Response(500, $headers, $body);
     }
 
     return $response;
