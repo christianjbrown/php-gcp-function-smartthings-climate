819e430765f40b741b99cbdd9b7efb00a86ea377
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-11-28 22:19:38 +0000

    Use device label instead of name for temperature output

--- src/DeviceTemperatureOutputTransformerInterface.php
+++ src/DeviceTemperatureOutputTransformerInterface.php
@@ -6,7 +6,7 @@ namespace ChristianBrown\GetSmartHomeTemps;
 
 interface DeviceTemperatureOutputTransformerInterface
 {
-    public const KEY_NAME = 'name';
+    public const KEY_LABEL = 'name';
     public const KEY_STALE = 'stale';
     public const KEY_TEMPERATURE = 'temp';
     public const KEY_TIMESTAMP = 'timestamp';
